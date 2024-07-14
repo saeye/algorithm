6aa0cb7a7074c0370ee69ac2c6c6b2cea10a41a6
t2023-m0134 <t2023-m0134@t2023-m0134ui-MacBookAir.local>
2024-07-14 15:29:57 +0900

    짝수의 합

--- codekata_answer.py
+++ codekata_answer.py
@@ -24,4 +24,29 @@ def plus_numbers(num1, num2):
 def solution(num1, num2):
     return int((num1 / num2)*1000)
 
+# 각도기
+def solution(angle):
+    angle = int(angle)
+    if 0 < angle < 90 :
+        return 1
+    elif angle == 90:
+        return 2
+    elif 90 < angle < 180:
+        return 3
+    elif angle == 180:
+        return 4
+    else:
+        return "angle은 0보다 크고 180 이하의 각입니다."
+
+# 짝수의 합(정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성하시오)
+def solution(n):
+    n = int(n)
+    even_sum = 0
+    for i in range(1, n+1): # range함수는 마지막 값 포함X
+        if i % 2 == 0:
+            even_sum += i
+        else:
+            even_sum
+    return even_sum
+
 
