a4d8fe094ba15337d8067b17b3a910db785317c1
t2023-m0134 <t2023-m0134@t2023-m0134ui-MacBookAir.local>
2024-07-19 16:58:34 +0900

    피보나치

--- codekata/test.py
+++ codekata/test.py
@@ -1,43 +1,22 @@
-import itertools
 def fibo(n):
     if n == 0:
-        return []
+        return f"0번째 수는 없음 None"
     elif n == 1:
-        return [1]
+        return 1
     elif n == 2:
-        return [1]
+        return 1
 
-    fibo_num = [1, 1]
-    for i in range():
-        fibo_num = fibo_num.append(fibo_num[n-1] + fibo_num[n])
+    fibo_num_list = [1, 1]
+    for i in range(2, n+1):
+        fibo_num_list.append(fibo_num_list[i-2] + fibo_num_list[-1])
 
+    return fibo_num_list[n-1]
 
+n = int(input(f"n번째 피보나치 수는? 숫자 n을 입력하세요"))
+print(fibo(n))
 
-
-
-
-# def fibo(n):
-#     fibo_num = [1, 1]
-#
-#     if n == 1 or n == 2:
-#         return 1
-#
-#     def fibo_numbers(n):
-#         fibo_num = [1, 1]
-#         if n == 0:
-#             return []
-#         elif n == 1:
-#             return [1]
-#         elif n == 2:
-#             return [1, 1]
-#         else:
-#             for i in fibo_num:
-#                 fibo_numbers(n += fibo_numbers(i)
-#
-#         print(fibo_numbers(1))
-
-#     fibo_num.append(fibo_num[n-3] + fibo_num[n-2])
-#     return fibo_num[n-1]
-#
-# print(fibo(4))
-
+# print(fibo(0)) # 0번째?: None
+# print(fibo(1)) # 1
+# print(fibo(5)) # 5
+# print(fibo(6)) # 8
+# print(fibo(10)) # 55
